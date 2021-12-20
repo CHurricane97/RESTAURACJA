f880328ee59923949b4dac4ccdcd5e112c046da8
CHurricane97 <barsul97@gmail.com>
2021-12-21 00:05:03 +0100

    skłądnik i pozycja w menu

---
+++ src/main/java/com/bd2pr/BD/Entities/Pozycja_w_menu.java
@@ -0,0 +1,50 @@
+package com.bd2pr.BD.Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Pozycja w menu")
+public class Pozycja_w_menu {
+    @Id
+    @Column(name = "Menu ID")
+    private Long menu_Id;
+
+    @Column(name = "Nazwa")
+    private String nazwa;
+
+    @Column(name = "Cena")
+    private Long cena;
+
+    public Long getMenu_Id() {
+        return menu_Id;
+    }
+
+    public void setMenu_Id(Long menu_Id) {
+        this.menu_Id = menu_Id;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public Long getCena() {
+        return cena;
+    }
+
+    public void setCena(Long cena) {
+        this.cena = cena;
+    }
+
+    @Override
+    public String toString() {
+        return "Pozycja_W_Menu{" +
+                "menu_Id=" + menu_Id +
+                ", nazwa='" + nazwa + '\'' +
+                ", cena=" + cena +
+                '}';
+    }
+}
---
+++ src/main/java/com/bd2pr/BD/Entities/Skladnik.java
@@ -0,0 +1,38 @@
+package com.bd2pr.BD.Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Skladnik")
+public class Skladnik {
+    @Id
+    @Column(name = "Skladnik ID")
+    private Long skladnik_Id;
+
+    @Column(name = "Nazwa")
+    private String nazwa;
+
+    public Long getSkladnik_Id() {
+        return this.skladnik_Id;
+    }
+
+    public void setSkladnik_Id(Long skladnik_Id) {
+        this.skladnik_Id = skladnik_Id;
+    }
+
+    public String getNazwa() {
+        return this.nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    @Override
+    public String toString() {
+        return "Skladnik{" +
+                "skladnik_Id=" + skladnik_Id +
+                ", nazwa='" + nazwa + '\'' +
+                '}';
+    }
+}
